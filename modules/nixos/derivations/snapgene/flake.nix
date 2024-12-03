a481cd16a66f4329b20a4fc224a51db270107e29
Alex Ryder <powerskater3@gmail.com>
2024-12-03 21:20:16 +0000

    Added a NixOS flake for deriving snapgene.

---
+++ modules/nixos/derivations/snapgene/flake.nix
@@ -0,0 +1,119 @@
+{
+  description = "Nix flake for the SnapGene software";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        permittedInsecurePackages = [ "openssl-1.1.1w" ];
+      };
+    };
+  in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation rec {
+      pname = "snapgene";
+      version = "8.0.1";
+      _pkgver_major = "8";
+      _pkgver_major_middle = "8.0";
+
+      src = pkgs.fetchurl {
+        url = "https://cdn.snapgene.com/downloads/SnapGene/${_pkgver_major}.x/${_pkgver_major_middle}/${version}/${pname}_${version}_linux.rpm";
+        sha256 = "10xqyrzybjy6dfgbzdhjyylm6xnwbqwccw1r1p1312j52cjydf32";
+      };
+
+      snapgeneWrapper = pkgs.writeScript "snapgene" ''
+        #!${pkgs.stdenv.shell}
+        exec "${placeholder "out"}/opt/gslbiotech/snapgene/snapgene.sh" "$@"
+      '';
+
+      nativeBuildInputs = [
+        pkgs.rpm
+        pkgs.autoPatchelfHook
+        pkgs.makeWrapper
+        pkgs.kdePackages.wrapQtAppsHook
+        pkgs.cpio
+      ];
+
+      buildInputs = with pkgs; [
+        qt6.qtbase
+        qt6.qtwebchannel
+        qt6.qt5compat
+        qt6.qtdeclarative
+        qt6.qtpositioning
+        qt6.qtsvg
+        qt6.qtwebengine
+        kdePackages.qca  # or kdeFrameworks.qca if kdeApplications.qca is unavailable
+        xorg.libSM
+        fontconfig
+        nspr
+        xz
+        expat
+        glibc
+        gcc
+        libcxx
+        libcxxrt
+        dbus
+        krb5
+        xorg.libxcb
+        libxkbcommon
+        xorg.xcbutil
+        xorg.libX11
+        xorg.libxkbfile
+        xorg.xcbutilkeysyms
+        xorg.xcbutilrenderutil
+        nss
+        hicolor-icon-theme
+        xorg.libICE
+        libglvnd
+        xorg.libXext
+        openssl_1_1  # Requires explicit permission due to being insecure
+        zlib
+        llvmPackages.openmp
+      ];
+
+      # Ensure autoPatchelfHook can find the binaries
+      autoPatchelfIgnoreMissingDeps = true;
+
+      unpackPhase = ''
+        rpm2cpio $src | cpio -idmv
+      '';
+
+      installPhase = ''
+        mkdir -p $out/opt/gslbiotech/snapgene
+        cp -r opt/gslbiotech/snapgene/* $out/opt/gslbiotech/snapgene/
+
+        # Modify snapgene.sh
+        sed -i 's#${placeholder "dollar"}{INSTALLED_DIR}/snapgene "$@"#QT_QPA_PLATFORM="xcb" ${placeholder "dollar"}{INSTALLED_DIR}/snapgene "$@"#' \
+          $out/opt/gslbiotech/snapgene/snapgene.sh
+
+        # Install the snapgene wrapper
+        mkdir -p $out/bin
+        install -m755 ${snapgeneWrapper} $out/bin/snapgene
+
+        # Create symlink to license
+        mkdir -p $out/share/licenses/$pname
+        ln -s $out/opt/gslbiotech/snapgene/resources/licenseAgreement.html \
+          $out/share/licenses/$pname/LICENSE.html
+
+        # Wrap the binary
+        makeWrapper $out/opt/gslbiotech/snapgene/snapgene.sh $out/bin/snapgene \
+          --set QT_QPA_PLATFORM "xcb" \
+          --set QT_PLUGIN_PATH "${pkgs.qt6.qtbase.dev}/lib/qt6/plugins"
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Software for plasmid mapping, primer design, and restriction site analysis";
+        homepage = "https://www.snapgene.com/about";
+        license = licenses.unfreeRedistributable;
+        maintainers = [];
+        platforms = platforms.linux;
+      };
+    };
+  };
+}
+
--- modules/nixos/packages/packages.nix
+++ modules/nixos/packages/packages.nix
@@ -1,37 +1,8 @@
 { pkgs, ... }:
 
-let
-  snapgeneVersion = "8.0.1";
-  snapgeneSha256 = "1lm607jk7i3zdlqj2rg3dhg6p1dshy4xygcxx8scnzh60l0q3xi0";
-  snapgene = pkgs.stdenv.mkDerivation {
-    pname = "snapgene";
-    version = snapgeneVersion;
-
-    src = pkgs.fetchurl {
-      url = "https://cdn.snapgene.com/downloads/SnapGene/8.x/8.0/8.0.1/snapgene_${snapgeneVersion}_linux.deb";
-      sha256 = snapgeneSha256;
-    };
-
-    unpackPhase = ''
-      ar x $src
-      tar -xf data.tar.xz
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin
-      ln -s $out/opt/gslbiotech/snapgene/snapgene $out/bin/snapgene
-    '';
-
-    meta = with pkgs.lib; {
-      description = "SnapGene - Molecular Biology Software";
-      homepage = "https://www.snapgene.com";
-      license = licenses.unfree;
-      platforms = platforms.linux;
-    };
-  };
-in{
+{
   environment.systemPackages = with pkgs; [
-      
+
       #Terminal-related
       rustc
       cargo
@@ -50,18 +21,11 @@ in{
       fastfetch
       os-prober
       powertop
-      
+
       #Office-related
       gimp
       inkscape
       libreoffice
-
-      #Compiling(?)
-      # autoconf
-      # automake
-      # libtool
-      # pkg-config
-      # m4
 
       #Development environments
       direnv
@@ -81,14 +45,15 @@ in{
       slurp
       swappy
       wlogout
+      jq
 
       #wireguard
       networkmanagerapplet
 
-      snapgene
   ];
 
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
   ];
 }
+
