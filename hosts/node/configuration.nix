be2703b0602cd9f874a24fc0fa2b3e959e761c3c
Alex Ryder <powerskater3@gmail.com>
2025-01-05 11:48:48 +0000

    Removed erroneous prime configuration on Node.

--- hosts/node/configuration.nix
+++ hosts/node/configuration.nix
@@ -117,15 +117,6 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.beta;
-
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
   };
   environment.systemPackages = with pkgs; [
     nvtopPackages.full
