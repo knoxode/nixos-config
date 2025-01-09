f631331b902c8355adacf97999a291139e19202c
Alex Ryder <powerskater3@gmail.com>
2025-01-09 21:51:47 +0000

    Added OpenRGB.

--- modules/nixos/environment/environment.nix
+++ modules/nixos/environment/environment.nix
@@ -1,8 +1,11 @@
+{ pkgs }:
+
 {
   environment.variables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
+  environment.systemPackages = with pkgs; [ openrgb-with-all-plugins ];
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
 }
