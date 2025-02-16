1c93de088ce827ecd8839b233722175a215c8f5b
Alex Ryder <powerskater3@gmail.com>
2025-02-16 19:31:36 +0000

    Added tigervnc and wayvnc

--- modules/nixos/packages/packages.nix
+++ modules/nixos/packages/packages.nix
@@ -75,6 +75,8 @@
     jq
     socat
     pywal
+    wayvnc
+    tigervnc
 
     # Miscellaneous tools
     brightnessctl
@@ -103,6 +105,27 @@
     #Fonts
     nerd-fonts.jetbrains-mono
     nerd-fonts.droid-sans-mono
+
+    # support both 32-bit and 64-bit applications
+    wineWowPackages.stable
+
+    # support 32-bit only
+    wine
+
+    # support 64-bit only
+    (wine.override { wineBuild = "wine64"; })
+
+    # support 64-bit only
+    wine64
+
+    # wine-staging (version with experimental features)
+    wineWowPackages.staging
+
+    # winetricks (all versions)
+    winetricks
+
+    # native wayland support (unstable)
+    wineWowPackages.waylandFull
   ];
 }
 
