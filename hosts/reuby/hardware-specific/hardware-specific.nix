ebd1d1d0c4bb24bbe94fae09186a1b0b3015921a
Alex Ryder <powerskater3@gmail.com>
2025-04-11 19:33:36 +0100

    Added fingerprint scanner to reuby. Not currently working.

--- hosts/reuby/hardware-specific/hardware-specific.nix
+++ hosts/reuby/hardware-specific/hardware-specific.nix
@@ -1,4 +1,16 @@
 { pkgs, lib, ... }:
 
 {
+  systemd.services.fprintd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.type = "simple";
+  };
+
+  services.fprintd = {
+    enable = true;
+    tod = {
+      enable = true;
+      driver = pkgs.libfprint-2-tod1-goodix;
+    };
+  };
 }
