f9cda5f2a914849196e06065bb4e1e365c22864d
Alex Ryder <powerskater3@gmail.com>
2025-04-18 17:09:42 +0100

    Added the most minor thing ever

--- hosts/nomad/hypr/monitor_script.sh
+++ hosts/nomad/hypr/monitor_script.sh
@@ -3,6 +3,10 @@ while ! pgrep Hyprland >/dev/null; do
   sleep 1
 done
 
+######################################################
+#                      SET PATHS                     #
+######################################################
+
 # Paths to Hyprland sockets
 EVENT_SOCKET="$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock"
 COMMAND_SOCKET="$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket.sock"
@@ -13,129 +17,14 @@ NVIDIA_CARD=$(readlink -f /dev/dri/by-path/*-card | tail -n 1)
 CONFIG_RELOAD_DONE=False
 MONITOR_INFO_STORE=/home/shaiikura/.hypr/monitor_info.json
 
-#Define monitor setters and getters
-
-#Set current
-set_monitor_id(){
-
-}
-
-set_monitor_name(){
-
-}
-
-set_monitor_description(){
-
-}
-
-set_monitor_width(){
-
-}
-
-set_monitor_height(){
-
-}
-
-set_monitor_scale(){
-
-}
-
-set_monitor_x(){
-  jq '(.[] | select(.name == "HDMI-A-1" and .description == "Samsung Electric Company SAMSUNG") | .x) = \"${MONITORXVALUE}\"' monitor_info.json
-}
-
-set_monitor_y(){
-
-}
-
-#Set previous
-set_monitor_id_last(){
-
-}
-
-set_monitor_name_last(){
-
-}
-
-set_monitor_description_last(){
-
-}
-
-set_monitor_width_last(){
-
-}
-
-set_monitor_height_last(){
-
-}
-
-set_monitor_scale_last(){
-
-}
-
-set_monitor_x_last(){
-
-}
-
-set_monitor_y_last(){
-
-}
-
-#GET current
-get_monitor_id(){
-
-}
-
-get_monitor_name(){
-
-}
-
-get_monitor_description(){
-
-}
-
-get_monitor_width(){
-
-}
-
-get_monitor_height(){
-
-}
-
-get_monitor_scale(){
-
-}
-
-get_monitor_x(){
-
-}
-
-get_monitor_y(){
-
-}
-
+#If the monitor_info file doesn't exist, make it.
 if [[ ! -e "$MONITOR_INFO_STORE" ]]; then
   touch MONITOR_INFO_STORE
 fi
 
-#Essentially the first node in the flow control scheme after an event is triggered
-#Only listening to the three events I care about.
-handle_events() {
-  case $1 in
-    monitoraddedv2*)
-      collect_added_monitor_info "$1"
-      ;;
-    monitorremoved*)
-      collect_removed_monitor_info "$1"
-      ;;
-    configreloaded*)
-      $CONFIG_RELOAD_DONE=True
-      ;;
-    moveworkspace*)
-      $CONFIG_RELOAD_DONE=True
-      ;;
-  esac
-}
+######################################################
+#            DEFINE ESSENTIAL FUNCTIONS              #
+######################################################
 
 # Function to collect added monitor information
 collect_added_monitor_info() {
@@ -155,9 +44,7 @@ collect_removed_monitor_info() {
   num_monitors=${#connected_monitors[@]}
 }
 
-file_check(){
-
-}
+file_check(){}
 
 file_read(){
 
@@ -171,7 +58,13 @@ single_monitor(){
 
 }
 
+######################################################
+#                    FINAL STAGE                     #
+#               EXECUTION FUNCTIONS                  #
+######################################################
+
 startup_function(){
+  #If the MONITOR_INFO_STORE file is empty, then fill it with the current monitor information.
   if [ ! -s "$MONITOR_INFO_STORE" ]; then
     hyprctl monitors -j | jq '[.[] | {id, name, description, model, favouredMode: .availableModes[0], width, height, refreshRate, x, y, scale, lastid, lastMode, lastwidth, lastheight, lastRefreshRate, lastx, lasty, lastscale}]' > $MONITOR_INFO_STORE
   fi
@@ -182,7 +75,24 @@ startup_function(){
 
 startup_function
 
-
+#Essentially the first node in the flow control scheme after an event is triggered
+#Only listening to the three events I care about.
+handle_events() {
+  case $1 in
+    monitoraddedv2*)
+      collect_added_monitor_info "$1"
+      ;;
+    monitorremoved*)
+      collect_removed_monitor_info "$1"
+      ;;
+    configreloaded*)
+      $CONFIG_RELOAD_DONE=True
+      ;;
+    moveworkspace*)
+      $CONFIG_RELOAD_DONE=True
+      ;;
+  esac
+}
 
 # Listen for events and process them
 socat - "UNIX-CONNECT:$EVENT_SOCKET" | while read -r line; do
