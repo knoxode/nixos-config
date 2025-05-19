0e45a8e581e6a4a3fda9842411723ba8542c9ed9
Alex Ryder <powerskater3@gmail.com>
2025-05-19 21:10:40 +0100

    Fix attempt 1 - Adding /profiles/intel and /core/drivers.

--- profiles/intel/default.nix
+++ profiles/intel/default.nix
@@ -1,7 +1,13 @@
-{ host, ... }:
+{host, ...}: {
   imports = [
     ../../hosts/${host}
     ../../modules/drivers
     ../../modules/core
   ];
+  # Enable GPU Drivers
+  drivers.amdgpu.enable = false;
+  drivers.nvidia.enable = false;
+  drivers.nvidia-prime.enable = false;
+  drivers.intel.enable = true;
+  vm.guest-services.enable = false;
 }
