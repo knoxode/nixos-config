4123c337568052537fb1b2bfb7aade729e7f3029
Alex Ryder <powerskater3@gmail.com>
2025-05-19 21:48:18 +0100

    Fuck nur.

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -10,22 +10,16 @@
       shaiikura = {
         id = 0;
         isDefault = true;
-        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-          dashlane
-          ublock-origin
-          enhancer-for-youtube
-          darkreader
+        extensions.packages = with pkgs; [
+          nur.repos.rycee.firefox-addons.dashlane
+          nur.repos.rycee.firefox-addons.ublock-origin
+          nur.repos.rycee.firefox-addons.enhancer-for-youtube
+          nur.repos.rycee.firefox-addons.darkreader
         ];
       };
       outlook = {
         id = 1;
         isDefault = false;
-        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-          dashlane
-          ublock-origin
-          enhancer-for-youtube
-          darkreader
-        ];
       };
     };
   };
