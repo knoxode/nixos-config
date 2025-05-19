075eb7b890833d59843061307974d895b6e2275c
Alex Ryder <powerskater3@gmail.com>
2025-05-19 23:38:53 +0100

    Added drivers from ZaneyOS.

--- modules/drivers/default.nix
+++ modules/drivers/default.nix
@@ -3,6 +3,11 @@
 }:
 {
   imports = [
+    ./amd-drivers.nix
     ./intel-drivers.nix
+    ./local-hardware-clock.nix
+    ./nvidia-drivers.nix
+    ./nvidia-prime-drivers.nix
+    ./vm-guest-services.nix
   ];
 }
