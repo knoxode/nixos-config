481cf1d2363bfbe72e01395faf1cc0f6591160c4
Alex Ryder <powerskater3@gmail.com>
2025-10-20 00:29:09 +0100

    Updated flake.lock.

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -16,7 +16,7 @@
         extensions.packages = with pkgs; [
           nur.repos.rycee.firefox-addons.dashlane
           nur.repos.rycee.firefox-addons.ublock-origin
-          #nur.repos.rycee.firefox-addons.enhancer-for-youtube
+          nur.repos.rycee.firefox-addons.enhancer-for-youtube
           nur.repos.rycee.firefox-addons.darkreader
           nur.repos.rycee.firefox-addons.tabliss
         ];
