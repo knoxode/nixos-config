5d6c6d1b21396b7964a36cef39131db341befe31
Alex Ryder <powerskater3@gmail.com>
2025-12-19 02:36:59 +0000

    Actually fixed rofi for all users now.

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -1,7 +1,6 @@
 {lib, ...}: {
   programs.rofi = lib.mkForce {
     enable = true;
-    configPath = "~/.config/rofi/config.rasi";
     theme = ./../../preferences/rofi/type-6-9.rofi;
     location = "center";
   };
