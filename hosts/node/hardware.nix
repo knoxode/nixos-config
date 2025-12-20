61602aad9de08140cbba3b7cde4a9660c267054d
Alex Ryder <powerskater3@gmail.com>
2025-12-20 23:22:07 +0000

    Set nodes kernel to cachyos kernel

--- hosts/node/hardware.nix
+++ hosts/node/hardware.nix
@@ -13,7 +13,7 @@
     extraModprobeConfig = ''
       options it87 force_id=0x8622
     '';
-    kernelPackages = pkgs.linuxPackages_zen;
+    kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto;
 
     loader = {
       grub = {
