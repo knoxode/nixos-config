5368c4e049ec6b8611cf82bacd21d5d253d23707
Alex Ryder <powerskater3@gmail.com>
2026-02-03 21:14:24 +0000

    Fixed the user-specific systemd configuration for Monitor Hotplugging systemd service.

--- modules/home/systemd.nix
+++ modules/home/systemd.nix
@@ -1,12 +1,29 @@
-{config, ...}: {
-  systemd.user.services = {
-    display_hotplug_monitor = {
-      Unit = {
-        Description = "A self-made display hotplugging monitor script";
-      };
-      Service = {
-        ExecStart = "/home/${config.home.username}/.config/monitorHotplug/displayHotplugMonitor.sh";
+{
+  host,
+  config,
+  ...
+}: let
+  inherit (import ./../../hosts/${host}/variables.nix) hostType;
+  displayHotPlugMonitor =
+    if hostType == "Desktop"
+    then {}
+    else {
+      displayHotPlugMonitor = {
+        Unit = {
+          Description = "A self-made display hotplugging monitor script";
+          After = ["hyprland-session.target"];
+          Requires = ["hyprland-session.target"];
+        };
+        Service = {
+          ExecStart = "/home/${config.home.username}/.config/monitorHotplug/displayHotplugMonitor.sh";
+          Restart = "always";
+          RestartSec = 5;
+        };
+        Install = {
+          WantedBy = ["hyprland-session.target"];
+        };
       };
     };
-  };
+in {
+  systemd.user.services = {} // displayHotPlugMonitor;
 }
