d779485105bf2b33d42bc9acd4162a21362f5c4b
Alex Ryder <powerskater3@gmail.com>
2026-02-06 00:51:50 +0000

    Cleaned up hypridle.nix a bit more. Easier to read.

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -1,55 +1,60 @@
 {...}: let
-  # Sets the values for timers in seconds.
-  shortestWait = 300;
-  shortWait = 600;
-  # medWait = 1800;
-  longWait = 3200;
-
   ddcScriptPath = ".local/bin/ddc-set-brightness-safe.sh";
   #Setting noctalia lock screen
   lock_cmd = "noctalia-shell ipc call lockScreen lock";
   suspend_cmd = "systemctl suspend ||  loginctl suspend";
+  before_sleep_cmd = "loginctl lock-session"; # Lock before suspend.
+  ignore_dbus_inhibit = false;
+
+  # Sets the values for timers in seconds.
+  shortestWait = 300; # 5 minutes
+  shortWait = 600; # 10 minutes
+  # medWait = 1800; # 30 minutes
+  longWait = 3200; # 60 minutes
+
+  #Grouping Listeners by wait time
+  shortestWaitListeners = [
+    {
+      timeout = shortestWait;
+      on-timeout = "loginctl lock-session"; # Lock screen when timeout has passed
+    }
+  ];
+
+  shortWaitListeners = [
+    {
+      timeout = shortWait;
+      on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # Turn off keyboard backlight.
+      on-resume = "brightnessctl -rd rgb:kbd_backlight"; # Turn on keyboard backlight.
+    }
+    {
+      timeout = shortWait;
+      on-timeout = ''brightnessctl -s set 10''; # set monitor backlight to minimum value, avoid 0 on OLED monitor.
+      on-resume = ''brightnessctl -r''; # Monitor backlight restore.
+    }
+    {
+      timeout = shortWait;
+      on-timeout = ''$HOME/${ddcScriptPath} down''; # set monitor backlight to minimum value, avoid 0 on OLED monitor.
+      on-resume = ''${ddcScriptPath} up''; # Monitor backlight restore.
+    }
+  ];
+
+  longWaitListeners = [
+    {
+      timeout = longWait;
+      on-timeout = suspend_cmd;
+    }
+  ];
 in {
   home.file."${ddcScriptPath}" = {
     source = ./ddcutilScripts/ddc-set-brightness-safe.sh;
     executable = true;
   };
 
-  services = {
-    hypridle = {
-      enable = true;
-      settings = {
-        general = {
-          inherit lock_cmd;
-          before_sleep_cmd = "loginctl lock-session"; # Lock before suspend.
-          ignore_dbus_inhibit = false;
-        };
-        listener = [
-          {
-            timeout = shortestWait;
-            on-timeout = "loginctl lock-session"; # Lock screen when timeout has passed
-          }
-          {
-            timeout = shortWait;
-            on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # Turn off keyboard backlight.
-            on-resume = "brightnessctl -rd rgb:kbd_backlight"; # Turn on keyboard backlight.
-          }
-          {
-            timeout = shortWait;
-            on-timeout = ''brightnessctl -s set 10''; # set monitor backlight to minimum value, avoid 0 on OLED monitor.
-            on-resume = ''brightnessctl -r''; # Monitor backlight restore.
-          }
-          {
-            timeout = shortWait;
-            on-timeout = ''$HOME/${ddcScriptPath} down''; # set monitor backlight to minimum value, avoid 0 on OLED monitor.
-            on-resume = ''${ddcScriptPath} up''; # Monitor backlight restore.
-          }
-          {
-            timeout = longWait;
-            on-timeout = suspend_cmd;
-          }
-        ];
-      };
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {inherit lock_cmd before_sleep_cmd ignore_dbus_inhibit;};
+      listener = shortestWaitListeners ++ shortWaitListeners ++ longWaitListeners;
     };
   };
 }
